25da78be8c055b1ac737bae0dd99fd200fcdc51b
Marc Neuhaus <apocalip@gmail.com>
2013-08-27 01:46:47 +0200

    Added automatic saving of ACF Fields to CustomFields.php

---
+++ CustomFields.php
@@ -0,0 +1,109 @@
+<?php
+	/**
+	 * Activate Add-ons
+	 * Here you can enter your activation codes to unlock Add-ons to use in your theme.
+	 * Since all activation codes are multi-site licenses, you are allowed to include your key in premium themes.
+	 */
+	function my_acf_settings( $options )
+	{
+	    // activate add-ons
+	    $options["activation_codes"]["repeater"] = "";
+	    $options["activation_codes"]["options_page"] = "";
+	    $options["activation_codes"]["flexible_content"] = "";
+	    $options["activation_codes"]["gallery"] = "";
+
+	    // setup other options (http://www.advancedcustomfields.com/docs/filters/acf_settings/)
+
+	    return $options;
+
+	}
+	add_filter("acf_settings", "my_acf_settings");
+	
+	/**
+	 * Register field groups
+	 * The register_field_group function accepts 1 array which holds the relevant data to register a field group
+	 * You may edit the array as you see fit. However, this may result in errors if the array is not compatible with ACF
+	 * This code must run every time the functions.php file is read
+	 */
+	if(function_exists("register_field_group"))
+	{
+		register_field_group(array(
+		'id' => 'acf_flexible-content',
+		'title' => 'Flexible Content',
+		'fields' => 
+		array(
+			0 => 
+			array(
+				'key' => 'field_521be1d024284',
+				'label' => 'Content',
+				'name' => 'content',
+				'type' => 'flexible_content',
+				'layouts' => 
+				array(
+					0 => 
+					array(
+						'label' => '50/50',
+						'name' => '50/50',
+						'display' => 'row',
+						'sub_fields' => 
+						array(
+							0 => 
+							array(
+								'key' => 'field_521be33b0a87b',
+								'label' => 'Left',
+								'name' => 'left',
+								'type' => 'text',
+								'column_width' => '',
+								'default_value' => '',
+								'placeholder' => '',
+								'prepend' => '',
+								'append' => '',
+								'formatting' => 'html',
+								'maxlength' => '',
+							),
+							1 => 
+							array(
+								'key' => 'field_521be3410a87c',
+								'label' => 'Right',
+								'name' => 'right',
+								'type' => 'text',
+								'column_width' => '',
+								'default_value' => '',
+								'placeholder' => '',
+								'prepend' => '',
+								'append' => '',
+								'formatting' => 'html',
+								'maxlength' => '',
+							),
+						),
+					),
+				),
+				'button_label' => 'Add Row',
+			),
+		),
+		'location' => 
+		array(
+			0 => 
+			array(
+				0 => 
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'page',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => 
+		array(
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => 
+			array(
+			),
+		),
+		'menu_order' => 0,
+	)); 
+	}
+	
