8d07113f6aa6e090ec86da602aa9aeea055f78ee
Marc Neuhaus <apocalip@gmail.com>
2013-09-19 13:32:08 +0200

    Added functionality to specify editors that should be removed inside the template
    Example:
    
    ```
    <?php
    /*
    Template Name: My Template
    Remove: editor, thumbnail
    */
    ?>
    ...
    ```

--- lib/custom.php
+++ lib/custom.php
@@ -9,13 +9,24 @@
  */
 function hide_editor() {
     // Get the Post ID.
-    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
-    if( !isset( $post_id ) ) return;
+    $postId = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+    if( !isset( $postId ) ) return;
 
     // Get the name of the Page Template file.
-    $template_file = get_post_meta($post_id, '_wp_page_template', true);
-    if ($template_file == 'page-content.php') {
-      remove_post_type_support('page', 'editor');
+    $templateFile = get_post_meta($postId, '_wp_page_template', true);
+    if (file_exists(__DIR__ . '/../' . $templateFile)) {
+        $template = file_get_contents(__DIR__ . '/../' . $templateFile);
+
+        $configuration = array('Remove' => '');
+        $headers = array_keys($configuration);
+        foreach (get_file_data(__DIR__ . '/../' . $templateFile, $headers) as $key => $value) {
+        	$configuration[$headers[$key]] = $value;
+        }
+
+        $features = explode(',', $configuration['Remove']);
+        foreach ($features as $feature) {
+        	remove_post_type_support('page', trim($feature));
+        }
     }
 }
 add_action('admin_init', 'hide_editor');
