4bf2b05f5425cf0d8659e9a945427232265b5902
Marc Neuhaus <apocalip@gmail.com>
2013-10-18 10:30:13 +0200

    Added page-sidebar.php

---
+++ page-sidebar.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Seite mit Sidebar
+ * Layout: Main
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?>
+
+<?php
+while (the_flexible_field('content')) {
+	$layout = get_row_layout();
+	$layout = ucfirst(str_replace('/', '_', $layout));
+	$template = 'Partials/' . $layout;
+	$templatePath = __DIR__ . '/Templates/' . $template . '.php';
+
+	if (!file_exists($templatePath)) {
+		file_put_contents($templatePath, '<strong>New empty Layout: ' . $template . '.php</strong>');
+	}
+
+	get_template_part('templates/' . $template);
+}
+?>
