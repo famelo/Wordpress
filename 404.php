e2e8a868b49a2724d7895ecb1315961f3fd14acd
Marc Neuhaus <apocalip@gmail.com>
2013-10-24 00:24:23 +0200

    Added a better 404

--- 404.php
+++ 404.php
@@ -1,11 +1,26 @@
-<div class="alert">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'famelo'); ?>
+<div class="container container-404">
+	<div class="row">
+		<div class="span12">
+			<div class="hero-unit center">
+				<h1>Seite nicht gefunden <small>404</small></h1>
+				<br />
+				<p>
+					Die Seite die sie versucht haben aufzurufen existiert leider nicht. <br />
+					Bitte kontrollieren Sie, ob sie die Webadresse richtig eingegeben haben.
+				</p>
+
+				<p>
+					Alternativ können Sie hier auch zur Startseite oder die ganze Seite durchsuchen:
+				</p>
+
+				<form role="search" method="get" id="searchform" class="form-search" action="/">
+					<a href="/" class="btn btn-default btn-home">Startseite</a>
+					<div class="search-wrapper">
+						<input type="text" name="s" class="form-control" placeholder="Seitentitel, Produktname, etc" />
+						<button type="submit" class="btn btn-default">Suchen</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
-
-<p><?php _e('It looks like this was the result of either:', 'famelo'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'famelo'); ?></li>
-  <li><?php _e('an out-of-date link', 'famelo'); ?></li>
-</ul>
-
-<?php get_search_form(); ?>
