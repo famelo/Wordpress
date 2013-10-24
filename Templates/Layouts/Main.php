b1c6f1d2ce0b1dda494b7780791961f2a8383002
ssteinker <svniii@gmx.de>
2013-10-24 13:27:14 +0200

    IE Support
    Added html5shim and X-UA-Compatible for IE

--- Templates/Layouts/Main.php
+++ Templates/Layouts/Main.php
@@ -4,6 +4,10 @@
   <meta charset="utf-8">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
 
   <?php wp_head(); ?>
 
